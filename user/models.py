4246558d38948765a7794b052eb4c05d89d85ac6
Pink Boy <qjf-king@foxmail.com>
2022-01-08 19:24:08 +0800

    更新用户的数据模型，增加常用字段

--- user/models.py
+++ user/models.py
@@ -3,8 +3,9 @@ uis系统数据模型
 """
 import uuid
 
-
 from django.db import models
+
+
 # Create your models here.
 
 
@@ -17,10 +18,187 @@ class BaseModel(models.Model):
     status = models.BooleanField(default=True, verbose_name="状态")
     is_delete = models.BooleanField(default=False, verbose_name="删除状态")
 
+    def __str__(self):
+        return self.status
+
+    class Meta:
+        ordering = ["create_date"]
+        verbose_name = "基础模型"
+        verbose_name_plural = "基础模型"
+
 
 class User(BaseModel):
     """
     用户基本数据模型
     """
     uid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, verbose_name="用户ID")
-    name = models.CharField(max_length=256, verbose_name="用户名")
+    account = models.CharField(max_length=256, unique=True, verbose_name="账号")
+    password = models.CharField(max_length=512, verbose_name="密码")
+    phone = models.CharField(max_length=256, verbose_name="手机号")
+
+    def __str__(self):
+        return self.account
+
+    class Meta:
+        ordering = ["create_date"]
+        verbose_name = "用户"
+        verbose_name_plural = "用户"
+
+
+class OptionType(BaseModel):
+    """
+    选项类型数据模型
+    """
+    type_name = models.CharField(max_length=256, verbose_name="类型名称")
+
+    def __str__(self):
+        return self.type_name
+
+    class Meta:
+        ordering = ["create_date"]
+        verbose_name = "选项类型"
+        verbose_name_plural = "选项类型"
+
+
+class Option(BaseModel):
+    """
+    选项数据模型
+    """
+    option_name = models.CharField(max_length=256, verbose_name="选项名称")
+    option_type = models.ForeignKey(
+        OptionType,
+        on_delete=models.CASCADE,
+        verbose_name="选项类型"
+    )
+
+    def __str__(self):
+        return self.option_name
+
+    class Meta:
+        ordering = ["create_date"]
+        verbose_name = "选项"
+        verbose_name_plural = "选项"
+
+
+class ImgType(BaseModel):
+    """
+    图片类型数据模型
+    """
+    type_name = models.CharField(max_length=256, verbose_name="图片类型名称")
+
+    def __str__(self):
+        return self.type_name
+
+    class Meta:
+        ordering = ["create_date"]
+        verbose_name = "图片类型"
+        verbose_name_plural = "图片类型"
+
+
+class Img(BaseModel):
+    """
+    图片数据模型
+    """
+    img_url = models.ImageField(verbose_name="图片链接")
+    img_type = models.ForeignKey(
+        ImgType,
+        on_delete=models.CASCADE,
+        verbose_name="图片类型"
+    )
+
+    def __str__(self):
+        return self.img_url
+
+    class Meta:
+        ordering = ["create_date"]
+        verbose_name = "图片"
+        verbose_name_plural = "图片"
+
+
+class Region(BaseModel):
+    """
+    地区数据模型
+    """
+    region_code = models.CharField(max_length=32, unique=True, verbose_name="行政编码")
+    region_name = models.CharField(max_length=64, verbose_name="地区名称")
+    region_short_name = models.CharField(max_length=32, null=True, verbose_name="地区缩写")
+    region_parent_id = models.ForeignKey(
+        "Region",
+        null=True,
+        on_delete=models.CASCADE,
+        verbose_name="地区父ID"
+    )
+    region_level = models.IntegerField(verbose_name="地区级别")
+
+    def __str__(self):
+        return self.region_name
+
+    class Meta:
+        ordering = ["create_date"]
+        verbose_name = "地区"
+        verbose_name_plural = "地区"
+
+
+class Address(BaseModel):
+    """
+    地址信息数据模型
+    """
+    region = models.ForeignKey(
+        Region,
+        on_delete=models.CASCADE,
+        verbose_name="地区"
+    )
+    address_info = models.CharField(max_length=256, verbose_name="详细地址")
+    zip_code = models.CharField(max_length=32, verbose_name="邮政编码")
+    address_tag = models.ForeignKey(
+        Option,
+        null=True,
+        on_delete=models.CASCADE,
+        verbose_name="地址标签"
+    )
+
+    def __str__(self):
+        return self.address_info
+
+    class Meta:
+        ordering = ["create_date"]
+        verbose_name = "地址信息"
+        verbose_name_plural = "地址信息"
+
+
+class UserInfo(BaseModel):
+    """
+    用户信息数据模型
+    """
+    user_id = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name="用户信息"
+    )
+    nick_name = models.CharField(max_length=256, verbose_name="用户昵称")
+    avatar = models.ForeignKey(
+        Img,
+        on_delete=models.CASCADE,
+        verbose_name="用户头像"
+    )
+    gender = models.ForeignKey(
+        Option,
+        on_delete=models.CASCADE,
+        verbose_name="性别选项"
+    )
+    age = models.IntegerField(verbose_name="用户年龄")
+    summary = models.CharField(max_length=512, verbose_name="用户简介")
+    email = models.EmailField(verbose_name="邮箱")
+    area = models.ForeignKey(
+        Address,
+        on_delete=models.CASCADE,
+        verbose_name="用户地区"
+    )
+
+    def __str__(self):
+        return self.nick_name
+
+    class Meta:
+        ordering = ["create_date"]
+        verbose_name = "用户信息"
+        verbose_name_plural = "用户信息"
